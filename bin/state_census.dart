9ab098a159f7f5633345e618bf41b2a9e905d035
Shradha1824 <shradhabj1824@gmail.com>
2021-09-19 07:19:19 -1200

    Loaded Indian State Census data

--- bin/state_census.dart
+++ bin/state_census.dart
@@ -1,3 +1,11 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:convert';
+import 'dart:io';
+
+void main(List<String> args) {
+  var stream = File('D:\StateCensusData.csv').openRead();
+  var buffer = StringBuffer();
+
+  stream.transform(utf8.decoder).listen((event) {
+    buffer.write(event);
+  }, onDone: () => print(buffer.toString()), onError: (e) => print(e));
 }
