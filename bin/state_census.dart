29c3b0a751803deb3f61c05a261d59a10f90a1ee
Shradha1824 <shradhabj1824@gmail.com>
2021-09-23 03:41:56 +0530

    Haldling exception

--- bin/state_census.dart
+++ bin/state_census.dart
@@ -2,31 +2,51 @@ import 'dart:convert';
 import 'dart:io';
 
 void main(List<String> args) {
-  var statecensusany = StateCensusAnalyser();
-  statecensusany.loadCsv();
-
-  var statecode = StateCodeAnalyser();
-  statecode.loadCsv();
+  var stateCensus = StateCensusAnalyser('', '', '', '');
+  stateCensus.loadStateCensusData();
 }
 
 class StateCensusAnalyser {
-  void loadCsv() {
-    var stream = File('D:\StateCensusData.csv').openRead();
-    var buffer = StringBuffer();
+  late String State;
+  late String Population;
 
-    stream.transform(utf8.decoder).listen((event) {
-      buffer.write(event);
-    }, onDone: () => print(buffer.toString()), onError: (e) => print(e));
+  late String AreaInSqKm;
+  late String DensityPerSqKm;
+  StateCensusAnalyser(
+      this.State, this.Population, this.AreaInSqKm, this.DensityPerSqKm);
+
+  StateCensusAnalyser.fromList(List<String> cols)
+      : this(cols[0], cols[1], cols[2], cols[3]);
+
+  @override
+  String toString() {
+    return 'StateCensusAnalyser{State:$State, Population : $Population, AreaInSqKm :$AreaInSqKm, DensityPerSqKm: $DensityPerSqKm}';
   }
-}
 
-class StateCodeAnalyser {
-  void loadCsv() {
-    var stream = File('D:\StateCode.csv').openRead();
-    var buffer = StringBuffer();
+  void loadStateCensusData() {
+    try {
+      final file = File('StateCensusData.csv');
+      Stream<List> inputStream = file.openRead();
+      inputStream.transform(utf8.decoder).transform(LineSplitter()).listen(
+          (String line) {
+        List row = line.split(',');
 
-    stream.transform(utf8.decoder).listen((event) {
-      buffer.write(event);
-    }, onDone: () => print(buffer.toString()), onError: (e) => print(e));
+        String State = row[0];
+        String Population = row[1];
+        String AreaInSqKm = row[2];
+        String DensityPerSqKm = row[3];
+
+        print('State: $State');
+        print('Population: $Population');
+        print('AreainSqkm: $AreaInSqKm');
+        print('DensityPerSqKm: $DensityPerSqKm');
+      }, onDone: () {
+        print('File is now closed.');
+      }, onError: (e) {
+        print(e.toString());
+      });
+    } catch (e) {
+      throw Exception();
+    }
   }
 }
