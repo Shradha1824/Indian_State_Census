844c7be0607f756dcf126d9ac204727794d09097
Shradha1824 <shradhabj1824@gmail.com>
2021-09-22 14:03:37 +0530

    Created class and using object call function

--- bin/state_census.dart
+++ bin/state_census.dart
@@ -2,10 +2,17 @@ import 'dart:convert';
 import 'dart:io';
 
 void main(List<String> args) {
-  var stream = File('D:\StateCensusData.csv').openRead();
-  var buffer = StringBuffer();
+  var statecensusany = StateCensusAnalyser();
+  statecensusany.loadCsv();
+}
 
-  stream.transform(utf8.decoder).listen((event) {
-    buffer.write(event);
-  }, onDone: () => print(buffer.toString()), onError: (e) => print(e));
+class StateCensusAnalyser {
+  void loadCsv() {
+    var stream = File('D:\StateCensusData.csv').openRead();
+    var buffer = StringBuffer();
+
+    stream.transform(utf8.decoder).listen((event) {
+      buffer.write(event);
+    }, onDone: () => print(buffer.toString()), onError: (e) => print(e));
+  }
 }
