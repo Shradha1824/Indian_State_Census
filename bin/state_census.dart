7b959cac8f6b7be85796ab9ce23dbead2f104fdd
Shradha1824 <shradhabj1824@gmail.com>
2021-09-22 14:45:21 +0530

    Loading State code Csv file

--- bin/state_census.dart
+++ bin/state_census.dart
@@ -4,11 +4,25 @@ import 'dart:io';
 void main(List<String> args) {
   var statecensusany = StateCensusAnalyser();
   statecensusany.loadCsv();
+
+  var statecode = StateCodeAnalyser();
+  statecode.loadCsv();
 }
 
 class StateCensusAnalyser {
   void loadCsv() {
     var stream = File('D:\StateCensusData.csv').openRead();
+    var buffer = StringBuffer();
+
+    stream.transform(utf8.decoder).listen((event) {
+      buffer.write(event);
+    }, onDone: () => print(buffer.toString()), onError: (e) => print(e));
+  }
+}
+
+class StateCodeAnalyser {
+  void loadCsv() {
+    var stream = File('D:\StateCode.csv').openRead();
     var buffer = StringBuffer();
 
     stream.transform(utf8.decoder).listen((event) {
